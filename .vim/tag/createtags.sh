c7d012a27c24d056b35d1baa3fe63f61fba48f7b
Sidney Chen <chenxilinsidney@gmail.com>
2014-12-26 16:15:41 +0800

    rewrite .vim files before clean it

--- .vim/tag/createtags.sh
+++
@@ -1,8 +0,0 @@
-# for opencv
-ctags -R --c++-kinds=+p -ICVAPI --fields=+iaS --extra=+q -f ~/.vim/tag/opencvtags ~/git_project/opencv/opencv/modules/
-# for system header files
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q -f ~/.vim/tag/systags  /usr/include/
-# for system header files
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q -f ~/.vim/tag/localsystags /usr/local/include/
-# for c++ library and STL
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q -f ~/.vim/tag/stdlibcpptags /usr/include/c++/4.8/
